c902797f7ffec30ccbc253e3487938f5cb4e6c24
Pamela Romina Ramírez <109164411+PamRz@users.noreply.github.com>
2022-10-23 15:14:51 -0300

    20/10
    20/10

--- ProyectoFinal_PamelaRamirez/src/app/app-routing.module.ts
+++ ProyectoFinal_PamelaRamirez/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//importamos componentes//
+import {PageOneComponent} from "./components/page-one.component";
 
 const routes: Routes = [];
 
